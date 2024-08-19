95b667c3cc08c59140eb9d51cea83842e91518f7
sandyspiers <sandy.spiers@curtin.edu.au>
2024-08-19 09:06:42 +0800

    default vimtex toc settings

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -7,7 +7,12 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
--- Vimtex pdf optiosn
+-- Vimtex
 -- vim.g.vimtex_view_method = "zathura" -- or "mupdf"
 vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+vim.g.vimtex_toc_config = {
+  show_help = 0,
+  todo_sorted = 0,
+  layer_status = { content = 1, label = 0, todo = 1, include = 1 },
+}

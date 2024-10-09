b39a7f8a47d4bbb307f72bbbb7bca6ee2a7e5f0b
sandyspiers <sandy.spiers@curtin.edu.au>
2024-10-09 16:34:55 +0800

    lsp precompile

--- jl-sys/create-sysimage.jl
+++ jl-sys/create-sysimage.jl
@@ -1,2 +1,5 @@
 using PackageCompiler
-PackageCompiler.create_sysimage(["OhMyREPL", "Revise"], sysimage_path="base.so", precompile_statements_file="base-precompile.jl")
+PackageCompiler.create_sysimage(#
+    ["OhMyREPL", "Revise"], sysimage_path="base.so", precompile_statements_file="base-precompile.jl")
+PackageCompiler.create_sysimage(#
+    ["LanguageServer", "SymbolServer"], sysimage_path="lsp.so", precompile_statements_file="lsp-precompile.jl")

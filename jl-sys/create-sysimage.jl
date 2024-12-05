92bedba296cd3c777d92b138e13ae11e85572dd6
sandyspiers <sandy.spiers@curtin.edu.au>
2024-12-05 10:07:14 +0800

    lazy images

--- jl-sys/create-sysimage.jl
+++ jl-sys/create-sysimage.jl
@@ -1,5 +1,13 @@
 using PackageCompiler
-PackageCompiler.create_sysimage(#
-    ["OhMyREPL", "Revise"], sysimage_path="base.so", precompile_statements_file="base-precompile.jl")
-PackageCompiler.create_sysimage(#
-    ["LanguageServer", "SymbolServer"], sysimage_path="lsp.so", precompile_statements_file="lsp-precompile.jl")
+
+PackageCompiler.create_sysimage(
+    ["OhMyREPL", "Revise"],
+    sysimage_path="base.so",
+    precompile_statements_file="base-precompile.jl"
+)
+
+PackageCompiler.create_sysimage(
+    ["LanguageServer", "SymbolServer"],
+    sysimage_path="lsp.so",
+    precompile_statements_file="lsp-precompile.jl"
+)

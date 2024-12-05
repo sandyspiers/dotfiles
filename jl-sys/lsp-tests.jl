78c16325b43a0d51dfe0be30632606f10179e44e
sandyspiers <sandy.spiers@curtin.edu.au>
2024-12-05 11:44:37 +0800

    update sys image creaters

--- jl-sys/lsp-tests.jl
+++ jl-sys/lsp-tests.jl
@@ -3,11 +3,13 @@ using TestEnv
 using LanguageServer
 using SymbolServer
 
-for mod in [LanguageServer, SymbolServer]
-    current_project = pwd()
-    _mod = pkgdir(mod)
-    Pkg.activate(_mod)
-    TestEnv.activate()
-    include(joinpath(_mod, "test", "runtests.jl"))
-    Pkg.activate(current_project)
+withenv("CI" => true) do
+    for mod in [LanguageServer, SymbolServer]
+        current_project = pwd()
+        _mod = pkgdir(mod)
+        Pkg.activate(_mod)
+        TestEnv.activate()
+        include(joinpath(_mod, "test", "runtests.jl"))
+        Pkg.activate(current_project)
+    end
 end

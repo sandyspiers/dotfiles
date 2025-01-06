9b0d250ddee0bbb5bb9e4a851111716eeca6b086
sandyspiers <sandy.spiers@gmail.edu.au>
2025-01-06 14:57:26 +0800

    REMOVE nvim

--- neovim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local quarto = require("quarto")
-quarto.setup()
-vim.keymap.set("n", "<leader>qp", quarto.quartoPreview, { desc = "Quarto preview", silent = true, noremap = true })

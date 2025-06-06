99ee400a0e498c17ad4dec483dd8167b0c12f69c
sandyspiers <sandy.spiers@gmail.com>
2025-06-06 09:53:54 +0800

    skip tests

--- juliaup/.julia/config/startup.jl
+++ juliaup/.julia/config/startup.jl
@@ -22,6 +22,8 @@ catch e
 end
 function include_tests()
     if isdir("test")
+        orig_skip_test = get(ENV, "SKIP_TEST", false)
+        ENV["SKIP_TEST"] = true
         println("includet(")
         for f in readdir("test"; join=true)
             if startswith(basename(f), "test") && endswith(basename(f), ".jl")
@@ -29,6 +31,7 @@ function include_tests()
                 includet(f)
             end
         end
+        ENV["SKIP_TEST"] = orig_skip_test
         println(")")
     else
         println("No test/ dir found.")

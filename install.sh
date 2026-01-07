ce9843ebf76563e96eaf61946c52708dcd086a7c
sandyspiers <sandy.spiers@gmail.com>
2026-01-07 08:46:38 +0800

    ask become pass

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ sudo pacman -S --noconfirm ansible
 echo "==> Running Ansible playbook..."
 cd ansible
 ansible-galaxy collection install -r requirements.yml
-ansible-playbook main.yml
+ansible-playbook main.yml --ask-become-pass
 
 echo "==> Done!"

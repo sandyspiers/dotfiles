66abe67c3034ecc616052aa463014332a3839537
sandyspiers <sandy.spiers@gmail.com>
2026-01-08 09:07:45 +0800

    add zellij plugs

---
+++ chezmoi/dot_config/zellij/executable_install_plugins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "=========================="
+echo "DOWNLOADING ZELLIJ PLUGINS"
+
+PLUG_DIR="$HOME/.config/zellij/plugins" 
+mkdir -p $PLUG_DIR
+cd $PLUG_DIR
+echo "Downloading to $(pwd)"
+
+echo "Downloading sandyspiers/zjpane"
+curl -L "https://github.com/FuriouZz/zjpane/releases/latest/download/zjpane.wasm" -o zjpane.wasm
+
+echo "Download dj95/zjstatus and dj96/zjframes"
+curl -L "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm" -o zjstatus.wasm
+curl -L "https://github.com/dj95/zjstatus/releases/latest/download/zjframes.wasm" -o zjframes.wasm
+
+echo "Download choosetree"
+curl -L "https://github.com/laperlej/zellij-choose-tree/releases/latest/download/zellij-choose-tree.wasm" -o zellij-choose-tree.wasm
+
+cd -
+echo "=========================="
+

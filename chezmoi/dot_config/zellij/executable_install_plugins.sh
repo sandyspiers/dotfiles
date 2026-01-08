b66393037c4dec5c250db270ad4e742939bcacdf
sandyspiers <sandy.spiers@gmail.com>
2026-01-08 11:05:08 +0800

    go back to my fork, it works at least

--- chezmoi/dot_config/zellij/executable_install_plugins.sh
+++ chezmoi/dot_config/zellij/executable_install_plugins.sh
@@ -9,7 +9,7 @@ cd $PLUG_DIR
 echo "Downloading to $(pwd)"
 
 echo "Downloading sandyspiers/zjpane"
-curl -L "https://github.com/FuriouZz/zjpane/releases/latest/download/zjpane.wasm" -o zjpane.wasm
+curl -L "https://github.com/sandyspiers/zjpane/releases/latest/download/zjpane.wasm" -o zjpane.wasm
 
 echo "Download dj95/zjstatus and dj96/zjframes"
 curl -L "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm" -o zjstatus.wasm

778437590b8d604a2edc15dd77aa47a0b683c0b4
sandyspiers <sandy.spiers@gmail.com>
2026-03-20 14:31:57 +0800

    drop repl auto indent

--- chezmoi/dot_julia/config/startup.jl
+++ chezmoi/dot_julia/config/startup.jl
@@ -1,33 +1,24 @@
 try
-    import REPL
-    REPL.GlobalOptions.auto_indent = false
-    REPL.LineEdit.options(::REPL.LineEdit.PromptState) = REPL.GlobalOptions
-catch e
-    println("Could not turn off auto-indenting!")
-end
-
-try
     import OhMyREPL
-    OhMyREPL.enable_autocomplete_brackets(false)
-catch e
+catch _
     println("Could not import OhMyREPL!")
 end
 
 try
     using Revise
-catch e
+catch _
     println("Could not import Revise!")
 end
 
 try
     using Infiltrator: @infiltrate
-catch e
+catch _
     println("Could not import Infiltrator!")
 end
 
 try
     using TestPicker
-catch e
+catch _
     println("Could not import TestPicker!")
 end
 

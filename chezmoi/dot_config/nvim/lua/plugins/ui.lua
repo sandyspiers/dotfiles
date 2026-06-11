d69514202adcef198f2cbf533a998d3d4d33af1d
sandyspiers <sandy.spiers@gmail.com>
2026-06-11 09:41:39 +0800

    change cursor colours

--- chezmoi/dot_config/nvim/lua/plugins/ui.lua
+++ chezmoi/dot_config/nvim/lua/plugins/ui.lua
@@ -6,6 +6,10 @@ return {
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme dracula]])
+      vim.opt.guicursor = "n-c:block-Cursor,i-ci:ver25-CursorInsert,v-ve:block-CursorVisual"
+      vim.api.nvim_set_hl(0, "Cursor", { fg = "#282A36", bg = "#FF79C6" })
+      vim.api.nvim_set_hl(0, "CursorInsert", { fg = "#282A36", bg = "#BD93F9" })
+      vim.api.nvim_set_hl(0, "CursorVisual", { fg = "#282A36", bg = "#50FA7B" })
     end,
   },
 

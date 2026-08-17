1a0382aed9f1530781e5ff123badf266e3daa091
sandyspiers <sandy.spiers@gmail.com>
2026-08-17 10:29:32 +0800

    add tpm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,6 +43,7 @@ install_tpm() {
     if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
         git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
     fi
+    "$HOME/.tmux/plugins/tpm/bin/install_plugins"
 }
 
 create_dirs() {
